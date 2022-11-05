f2fcc68b163178f3d2ed8a076bc8b90ce8a74063
Gaidahaudatuljannah <117494887+Gaidahaudatuljannah@users.noreply.github.com>
2022-11-05 11:11:42 +0800

    Add files via upload

---
+++ pbo/Latihan5.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author gaida
+ */
+public class Latihan5 {
+     public static void main(String[] args) {
+        String nama;
+        Scanner inputan = new Scanner(System.in).useDelimiter("\n");
+        System.out.println("Masukkan Nama Lengkap Anda :");
+        nama = inputan.next();
+        System.out.println("Halo... GAIDA HAUDATUL JANNAH"+nama+", Selamat Belajar Java");
+}    
+}
+   
+
